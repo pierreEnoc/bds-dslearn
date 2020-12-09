7c3770c54236af99dad8dd6193e0f208316723f2
pierreEnoc <pierre_enoc@yahoo.fr>
2020-12-09 14:48:13 -0300

    :sparkles: validation Exception handling, get user

--- backend/src/main/java/com/dev/pierre/dslearn/services/UserService.java
+++ backend/src/main/java/com/dev/pierre/dslearn/services/UserService.java
@@ -1,5 +1,7 @@
 package com.dev.pierre.dslearn.services;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +9,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.dev.pierre.dslearn.dto.UserDTO;
 import com.dev.pierre.dslearn.entities.User;
 import com.dev.pierre.dslearn.repositories.UserRepository;
+import com.dev.pierre.dslearn.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -19,7 +24,13 @@ public class UserService implements UserDetailsService {
 	@Autowired
 	private UserRepository userRepository;
 	
-
+	@Transactional(readOnly = true)
+	public UserDTO findById(Long id) {
+		Optional<User> obj = userRepository.findById(id);
+		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new UserDTO(entity);
+	}
+	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		User user = userRepository.findByEmail(username);
