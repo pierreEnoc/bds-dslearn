321d12bb906edd4b288ae8bf1514390ab1529478
pierreEnoc <pierre_enoc@yahoo.fr>
2020-12-12 21:09:25 -0300

    :sparkles: Customized content for logged in user

--- backend/src/main/java/com/dev/pierre/dslearn/repositories/NotificationRepository.java
+++ backend/src/main/java/com/dev/pierre/dslearn/repositories/NotificationRepository.java
@@ -1,9 +1,21 @@
 package com.dev.pierre.dslearn.repositories;
 
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.dev.pierre.dslearn.entities.Notification;
+import com.dev.pierre.dslearn.entities.User;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+	
+	@Query("SELECT obj FROM Notification obj WHERE" 
+			+ "(obj.user = :user) AND "
+			+ "(obj.read = :read)"
+			+ "ORDER BY obj.moment DESC")
+	Page<Notification> find(User user, boolean read, Pageable pageable);
 
 }
---
+++ backend/src/main/java/com/dev/pierre/dslearn/services/NotificationService.java
@@ -0,0 +1,29 @@
+package com.dev.pierre.dslearn.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.dev.pierre.dslearn.dto.NotificationDTO;
+import com.dev.pierre.dslearn.entities.Notification;
+import com.dev.pierre.dslearn.entities.User;
+import com.dev.pierre.dslearn.repositories.NotificationRepository;
+
+@Service
+public class NotificationService {
+	
+	@Autowired
+	private NotificationRepository notificationRepository;
+	
+	@Autowired
+	private AuthService authService;
+	
+	public Page<NotificationDTO> notificationsForCurrentUser(Pageable pageable) {
+		User user = authService.authenticated();
+		Page<Notification> page = notificationRepository.find(user, false, pageable);
+		return page.map(x -> new NotificationDTO(x));
+		
+	}
+
+}
